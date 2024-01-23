6c1b0e643a21d1d957951477b27d21dd84a16d72
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2024-01-23 12:30:37 +0100

    Classe Componente

--- Classi/Componente.cs
+++ Classi/Componente.cs
@@ -9,6 +9,11 @@ namespace Classi
 {
     public abstract class Componente
     {
-
+        public abstract void Add(Componente componente);
+        public abstract void Remove(int indice);
+        public abstract Componente GetChild(int indice);
+        public abstract string ToString();
+        public abstract bool Equals(object obj);
+        public abstract int Punteggio();
     }
 }

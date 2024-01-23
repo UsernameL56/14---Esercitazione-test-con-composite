dd4f7c10902e96ae4247885052cc70aaa8325b72
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2024-01-23 12:30:23 +0100

    Aggiungere i file di progetto.

---
+++ 14 - Esercitazione test con composite/Form1.cs
@@ -0,0 +1,15 @@
+namespace _14___Esercitazione_test_con_composite
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}

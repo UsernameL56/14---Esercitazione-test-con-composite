6ac3a2c4a5bbae4351605b31ea5d75ef966022db
Corsi.20188 <Corsi.20188@E2-9PCWS2>
2024-01-23 12:38:41 +0100

    Classe Test

---
+++ Classi/Test.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classi
+{
+    public class Test : Componente
+    {
+        // attributi
+        List<Componente> _componenti = new List<Componente>();
+
+        // metodi accessor
+        public List<Componente> Componenti
+        {
+            get { return _componenti; }
+            set { _componenti = value; }
+        }
+
+        // costruttore
+        public Test(List<Componente> _componenti)
+        {
+            Componenti = _componenti;
+        }
+
+        // metodi
+        public override void Add(Componente componente)
+        {
+            Componenti.Add(componente);
+        }
+        public override void Remove(int indice)
+        {
+            if (indice >= 0 && indice < Componenti.Count)
+                Componenti.RemoveAt(indice);
+        }
+        public override Componente GetChild(int indice)
+        {
+            if(indice < Componenti.Count)
+                return Componenti[indice];
+            else
+                return null;
+        }
+        public override string ToString()
+        {
+            string stringa = "";
+            foreach(Componente componente in Componenti)
+            {
+                stringa += componente.ToString();
+            }
+
+            return stringa;
+        }
+    }
+}
